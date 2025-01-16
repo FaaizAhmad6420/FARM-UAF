2e102f6ccfe985611f466fc61fdc9e14c726ab7c
FaaizAhmad6420 <faizahmad6420@gmail.com>
2025-01-16 18:02:29 +0500

    update product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,9 +2,9 @@ class Product < ApplicationRecord
   include Ransackable
 
   belongs_to :category
-  has_many :carts
+  has_many :carts, dependent: :destroy
   has_many :product_sizes, dependent: :destroy
-  has_many :orders, through: :carts
+  has_many :orders, through: :carts, dependent: :destroy
   has_many :sizes, through: :product_sizes
   accepts_nested_attributes_for :product_sizes, allow_destroy: true
   has_one_attached :image
